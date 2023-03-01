c47ee69eee44ce4fa680289a3244bd7ae2c675f0
Ji Sison <jisison.dev@gmail.com>
2023-03-01 22:09:17 +0800

    debug

--- deterministic-finite-automata-state-diagram-gen/dfa.py
+++ deterministic-finite-automata-state-diagram-gen/dfa.py
@@ -22,61 +22,60 @@ class DFA():
         
     """
 
+    def __init__(self, states = set(), alphabet = set(), transition = dict(), start_state = None, accept_states = set()):
+        """
+        Class initialization
+        """
+        self.states = set(states)
+        self.alphabet = set(alphabet)
+        self.transition = transition
+        self.start_state = start_state
+        self.accept_states = set(accept_states)
 
-def __init__(self, states = set(), alphabet = set(), transition = dict(), start_state = None, accept_states = set()):
-    """
-    Class initialization
-    """
-    self.states = set(states)
-    self.alphabet = set(alphabet)
-    self.transition = transition
-    self.start_state = start_state
-    self.accept_states = set(accept_states)
+    def __repr__(self):
+        """
+        Class representation
+        """
+        return "Deterministic Finite Automata (DFA) at " +f"{hex(id(self))}"
 
-def __repr__(self):
-    """
-    Class representation
-    """
-    return "Deterministic Finite Automata (DFA) at " +f"{hex(id(self))}"
+    def __str__(self):
+        """
+        Class string representation
+        """
+        str_self = (
+            self.__repr__() + "\n"
+            + "States: " + f"{self.states}\n"
+            + "Symbols: " + f"{self.alphabet}\n"
+            + "Start States: " + f"{self.start_state}\n"
+            + "Accept: " + f"{self.accept_states}\n"
+            + self.str_transition()
+            )
+        return str_self
 
-def __str__(self):
-    """
-    Class string representation
-    """
-    string = (
-        self.__repr__() + "\n"
-        + "States: " + f"{self.states}\n"
-        + "Symbols: " + f"{self.alphabet}\n"
-        + "Start States: " + f"{self.start_state}\n"
-        + "Symbols: " + f"{self.accept_states}\n"
-        + self.str_transition()
-        )
-    return str_self
+    def str_transition(self):
+        """
+        String representation of the transition function
+        """
 
-def str_transition(self):
-    """
-    String representation of the transition function
-    """
+        string = " Transitions: state, symbol -> state"
+        for state in self.transition:
+            state_transitions = self.transition[state]
+            for symbol in self.alphabet:
+                string += "\n\t" + state + " , " + symbol + "->" + state_transitions[symbol]
+        return string
 
-    string = " Transitions: state, symbol -> state"
-    for state in self.transition:
-        state_transitions = self.transition[state]
-        for symbol in self.alphabet:
-            string += "\n\t" + state + "," + symbol + "->" + state_transitions[symbol]
-    return string
+    def accepts(self, input_string = ""):
+        """
+        Returns True if DFA accept the input string (default is the empty string) otherwise False
 
-def accepts(self, input_string = "");
-    """
-    Returns True if DFA accept the input string (default is the empty string) otherwise False
+        """
+        current_state = self.start_state
+        for idx in range(len(input_string)):
+            symbol = input_string[idx]
+            current_state = self.transition[current_state][symbol]
+        if current_state in self.accept_states:
+            return True
+        else:
+            return False
 
-    """
-    current_state = self.start_state
-    for idx in range(len(input_string)):
-        symbol = input_string[idx]
-        current_state = self.transition[current_state][symbol]
-    if current_state in self.accept_states:
-        return True
-    else:
-        return False
-
-        # graphvis
+            # graphvis
